abb0f61001ee36970615988c0b1d5024ad29b38d
yichihchen <ingridiaim@gmail.com>
2022-11-26 10:22:32 +0800

    背景重疊

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,9 +1,14 @@
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
 /*進入動畫*/
 gsap.from(".banner-container", { y: 1000, duration: 1.5 }); //banner滑入
-/*分鏡動畫*/
 
+/*阿兵高冒出來-分鏡動畫*/
+/*滑鼠滑到才冒出來*/
+const tl = gsap.timeline();
+tl.from(".red-duke", { xPercent: 50, yPercent: 50, duration: 1.5 });
+tl.from(".dialogue2", {xPercent:20,scale:0.5,duration: 1});/*對話框縮小到放大,並從右下角開始變大*/
 
+/*打字動畫*/
 gsap.to(".typing1", {
     text: "羨慕別人的酷酷網頁動畫...",
     duration: 1.5,
@@ -35,9 +40,9 @@ gsap.fromTo(".cursor", 0, {
     visibility: "hidden",
 },
     {
-        visibility:"visible",
-        repeat:-1,
-        yoyo:true,
-        repeatDelay:0.3,
+        visibility: "visible",
+        repeat: -1,
+        yoyo: true,
+        repeatDelay: 0.3,
     }
 )
